1af92427caf080b08c305e59705681bbc8fac3bb
ChristianAlameda <jazzbilbo@gmail.com>
2023-05-06 19:00:18 -0700

    added images and some code to load the images

---
+++ map.py
@@ -0,0 +1,15 @@
+import pygame
+import os
+board = pygame.transform.scale2x(pygame.image.load(os.path.join("chessBoard.png")))
+
+b_bishop = pygame.image.load(os.path.join("black","BBishop.png"))
+b_knight = pygame.image.load(os.path.join("black","BKnight.png"))
+b_pawn = pygame.image.load(os.path.join("black","BPawn.png"))
+b_king = pygame.image.load(os.path.join("black","BKing.png"))
+b_queen = pygame.image.load(os.path.join("black","BQueen.png"))
+
+w_bishop = pygame.image.load(os.path.join("white","WBishop.png"))
+w_knight = pygame.image.load(os.path.join("white","WKnight.png"))
+w_pawn = pygame.image.load(os.path.join("white","WPawn.png"))
+w_king = pygame.image.load(os.path.join("white","WKing.png"))
+w_queen = pygame.image.load(os.path.join("white","WQueen.png"))
