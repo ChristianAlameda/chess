354da5ce9658f7c3e041f2653a640188f1bf1f7f
ChristianAlameda <jazzbilbo@gmail.com>
2023-05-06 19:19:37 -0700

    added a class for pieces then for all pieces to have their own rules

---
+++ piece.py
@@ -0,0 +1,30 @@
+class Piece:
+    def __init__(self):
+        pass
+    def move(self):
+        pass
+    def capture(self):
+        pass
+    def check_illegal_move(self):
+        pass
+class Pawn(Piece):
+    def move(self):
+        pass
+    def capture(self):
+        pass
+    def check_illegal_move(self):
+        pass
+class Knight(Piece):
+    def move(self):
+        pass
+    def capture(self):
+        pass
+    def check_illegal_move(self):
+        pass
+class Bishop(Piece):
+    def move(self):
+        pass
+    def capture(self):
+        pass
+    def check_illegal_move(self):
+        pass
