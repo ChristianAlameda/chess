9aadf566b5b8da3411a831d0b2eef95b79df024c
ChristianAlameda <jazzbilbo@gmail.com>
2023-05-11 11:42:18 -0700

    We added the ability to see the board :)

--- board.py
+++ board.py
@@ -1,8 +1,19 @@
 import pygame
-import map
+import mapping
 class Board:
     def init(self):
-        self.board = map.board
+        self.board = mapping.board
     def create_board(self):
-        pygame.display(map.board)
-        
+        # sets the height and width of the board with (x,y)
+        x = 1000
+        y = 1000
+        background = pygame.display.set_mode((x, y))
+        image = mapping.board
+
+        while True:
+            for i in pygame.event.get():
+                if i.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+            background.blit(image, (150, 100))
+            pygame.display.update()
